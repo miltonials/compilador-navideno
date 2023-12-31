1944b8f6004ccd4c0637ffb14083de94c8c7235f
Milton Barrera <miltonjosue2001@gmail.com>
2023-12-30 21:47:44 -0600

    Tipado fuerte para retorno de funciones

--- src/test/java/com/compi/ParserTest.java
+++ src/test/java/com/compi/ParserTest.java
@@ -7,45 +7,37 @@ import java_cup.runtime.Symbol;
 
 
 public class ParserTest {
-  private String funMain = "function int main () { local int res <= 1+3|return 3|}";
-  private String funStr = "function string hola () {local string res <= \"hola\"|return 3|}" + funMain;
+  private String funMain = "function int main () { local int res <= 1+3|return res|@comentario\n}";
+  private String funStr = "function string hola () {local string res <= \"hola\"|return \"3\"|}" + funMain;
   private String funInt = "function int hola () {local int res <= 1+3|return 3|}" + funMain;
-  private String funBool = "function bool hola () {local int res <= 1+3|return 3|}" + funMain;
+  private String funBool = "function bool hola () {local int res <= 1+3|return true|}" + funMain;
 
+  private Symbol testHelper (String christmassCode) throws Exception {
+    IdLexer lexer = new IdLexer(new StringReader(christmassCode));
+    Parser p = new Parser(lexer);
+    return p.parse();
+  }
   @Test
   public void testMain() throws Exception {
-    String expresion = funMain;
-    IdLexer lexer = new IdLexer(new StringReader(expresion));
-    Parser p = new Parser(lexer);
-    Integer resultado = (Integer) p.parse().value;
+    Integer resultado = (Integer) testHelper(funMain).value;
     System.out.println(resultado);
   }
 
   @Test
   public void testStr() throws Exception {
-    String expresion = funStr;
-    IdLexer lexer = new IdLexer(new StringReader(expresion));
-    Parser p = new Parser(lexer);
-    Integer resultado = (Integer) p.parse().value;
+    Integer resultado = (Integer) testHelper(funStr).value;
     System.out.println(resultado);
   }
 
   @Test
   public void testInt() throws Exception {
-    String expresion = funInt;
-    IdLexer lexer = new IdLexer(new StringReader(expresion));
-    Parser p = new Parser(lexer);
-    Integer resultado = (Integer) p.parse().value;
+    Integer resultado = (Integer) testHelper(funInt).value;
     System.out.println(resultado);
   }
 
   @Test
   public void testBool() throws Exception {
-    String expresion = funBool;
-    System.out.println(expresion);
-    IdLexer lexer = new IdLexer(new StringReader(expresion));
-    Parser p = new Parser(lexer);
-    Symbol resultado = (Symbol) p.parse();
+    Integer resultado = (Integer) testHelper(funBool).value;
     System.out.println(resultado);
   }
 }
