928612227af15c47d5e95a947c166ed16b09b647
Milton Barrera <miltonjosue2001@gmail.com>
2024-01-04 01:26:50 -0600

    Restricción para los tipos de arreglos que se pueden crear. Op_Aritméticas con variables

--- src/test/java/com/compi/ParserTest.java
+++ src/test/java/com/compi/ParserTest.java
@@ -11,7 +11,7 @@ public class ParserTest {
   private String funMain = "function int main () { local int res <= 1+3|return res|@comentario\n}";
   private String funStr = "function int hola () {local string res <= \"hola\"|return 3|}";
   private String funInt = "function int hola () {";
-  private String funfloat = "function int hola (int x, char y) {";
+  private String funfloat = "function float hola (int x, char y) {";
   private String funBool = "function bool hola () {";
   private String funChar = "function char hola () {";
   private String funArr = "function array hola () {";
@@ -63,22 +63,17 @@ public class ParserTest {
   @Test
   public void testFloat() throws Exception {
     String program = funfloat;
-    program += "local float multiUnario <= 5 + ++--++1.1|";
+    program += "local float multiUnario <= 5.1 + 1.1|";
     program += "local float res <= 1.3|";
     program += "local float res1 <= ++(1.34)|";
     program += "local float res2 <= ++(((1.3)*(0.3)))|";
-    program += "local float res3 <= ++((1))|";
-    program += "local float res4 <= ++((1) - 23+1**2*5)|";
-    program += "local float res5 <= (1+3)|";
-    program += "local float res6 <= res1|";
-    program += "local float res7 <= (res1)|";
-    program += "local float res8 <= res1 + res2 + res3|";
-    program += "local float res9 <= ++((res1) - 23+res3**res6*5)|";
-    program += "local float res10 <= ((res1) - 23+res3**res6*5)|";
-    program += "local float res11 <= ((res1) - 23.6+res3**res6*5) + 4|";
-    program += "local float res11 <= ++((res1) - 23+res3**res6*5) + 4|";
-    program += "local float res11 <= 3 - ((res1) - 23.4+res3**res6*5) + 4|";
-    program += "return ((res1)- ((23))+res3**res6*-5.4)|}";//res1+res2+res3
+    program += "local float res3 <= ++((1.1))|";
+    program += "local float res4 <= ++((1.1) - 23.1 + 1.0**2.0*5.0)|";
+    program += "local float res5 <= (1.3)|";
+    program += "local float res6 <= res1*1.0|";
+    program += "local float res7 <= (res1*1.0)|";
+    program += "local float res8 <= res1*1.0 + res2*1.0 + res3*1.0|";
+    program += "return (res*1.1 - ((23.1)) + ((res3 *1.2) ** res6 *1.3) * -5.4)|}";//res1+res2+res3
     //program += "return 3.4|}";
     program += funMain;
     
@@ -93,6 +88,7 @@ public class ParserTest {
     program += "local bool res2 <= true|";
     program += "local bool res3 <= true ^ false|";
     program += "local bool res4 <= true # false|";
+    program += "local int res5 <= 12 + 21|";
     //program += "local bool res3 <= res1|";
     //program += "local bool res4 <= res1 && res2|";
     //program += "local bool res5 <= res1 || res2|";
@@ -129,11 +125,9 @@ public class ParserTest {
     program += "local array x <= [1]|";
     program += "local int m|";
     program += "m <= [123]|";
-    program += "local array y <= [[2],[[2],[2]]]|";
-    program += "local array z <= [\"d\"]|";
+    program += "local array y <= [2, tres, cuatro]|";
     program += "local array w <= [variable, 3]|";
-    program += "local array w <= [variable, \"string\"]|";
-    program += "local array w <= [[1], variable]|";
+    program += "local array w <= [mil, variable]|";
     program += "return 1|}";
     program += funMain;
 
@@ -192,7 +186,7 @@ public class ParserTest {
     String program = funChar + "\n";
     program += "local array x <= [1]|";
     //for
-    program += "for (local int res <= 1| 2<5 | ++i) {";
+    program += "for (local int res <= 1| 2<5 | ++res) {";
     program += "local int m|";
 
     program += "if (true) {";
@@ -228,6 +222,30 @@ public class ParserTest {
     program += "local string identificador <= read(\"ingrese algo:\")|";
 
     program += "local array x <= [1]|";
+    program += "return 1|}";
+    program += funMain;
+
+    Integer resultado = (Integer) testHelper(program).value;
+    System.out.println(resultado);
+  }
+
+  @Test
+  public void miTest() throws Exception {
+    String program = funInt;
+
+    //program += "local int x <= 21 + 1|";
+    program += "local int miVariable <= 13|";
+    program += "local int m <= miVariable + x ** x + 1|";
+    program += "local int a <= 12 + x + 1|";
+
+    program += "local float x <= 2.1 + 1.0|";
+    program += "local float miVariable <= 1.3|";
+    program += "local float m <= miVariable*1.0 + x*1.0 ** x*1.0 + 1.1|";
+    program += "local float a <= 12.1 + x*1.0 + 2.0 |";
+    program += "local float a <= (x*1.0 + y*1.0)|";
+    program += "local float a <= (x*1.0 + y*1.0 + 1.0 + y*1.0) + 1.0|";
+
+
     program += "return 1|}";
     program += funMain;
 
