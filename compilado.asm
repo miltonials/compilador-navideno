8f46bdaa3d1ec891249fa740944e32e77abd865e
Andy Porras <andyporras@estudiantec.cr>
2024-01-20 17:37:06 -0600

    MIPS | Declaración de variable Array

--- compilado.asm
+++ compilado.asm
@@ -2,30 +2,15 @@
 .data
 
 saltoLinea: .asciiz "\n"
-dhola: .word 0
-chola: .word 0
-holamain: .word 0
-hola2main: .float 0.0
+arrmain: .space 12
+dmain: .word 0
 
 .text
 
-hola:
-#5
-sw $t0, dhola
-li $t0, 0
 main:
-#5
-sw $t0, holamain
+#0
+sw $t0, dmain
 li $t0, 0
-#3.3
-s.s $f0, hola2main
-l.s $f12, hola2main
-li $v0, 2
-syscall
-li $t0, 0
-li $a0, hola
-li.s $f1, hola2
-jal hola
 
 j end_program
 
