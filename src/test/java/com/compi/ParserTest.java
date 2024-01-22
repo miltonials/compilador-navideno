127db969db49541f7abe79c40e87bd584b91089a
Milton Barrera <miltonjosue2001@gmail.com>
2024-01-21 20:07:28 -0600

    MIPS | Ciclo for

--- src/test/java/com/compi/ParserTest.java
+++ src/test/java/com/compi/ParserTest.java
@@ -42,21 +42,22 @@ public class ParserTest {
   public void testInt() throws Exception {
     String program = funInt;
     program += "local int res <= 1|";
-    program += "local int res1 <= ++(1)|";
+    program += "local int res1 <= (1)|";
 
-    program += "local int res2 <= ++(((1)*(2)))|";
+    program += "local int res2 <= (((1)*(2)))|";
 
-    program += "local int res3 <= ++((1))|";
-    program += "local int res4 <= ++((1)- 23+1**2*5)|";
+    program += "local int res3 <= ((1))|";
+    program += "local int res4 <= ((1)- 23+1**2*5)|";
     program += "local int res5 <= (1+3)|";
     program += "local int res6 <= res1|";
     program += "local int res7 <= (res1)|";
     program += "local int res8 <= res1 + res2 + res3|";
-    program += "local int res9 <= ++((res1)+ -23+res3**res6*5)|";
+    program += "local int res9 <= ((res1)+ -23+res3**res6*5)|";
     program += "local int res10 <= ((res1)- 23+res3**res6*5)|";
     program += "local int res11 <= ((res1)- 23+res3**res6*5) + 4|";
-    program += "local int res11 <= ++((res1)+ (-23)+res3**res6*5) + 4|";
-    program += "local int res11 <= 3 - ((res1)+ 23+res3**res6*5)*-1 + 4|";
+    program += "local int res11 <= ((res1)+ (-23)+res3**res6*5) + 4|";
+    program += "local int res11 <= 3 - ((res1)+ 23+res3**res6*5) - 1 + 4|";
+
     program += "return ((res1)- 23+res3**res6*5)|}";//res1+res2+res3
 
     //program += "return 3|}";
@@ -68,30 +69,9 @@ public class ParserTest {
   @Test
   public void testFloat() throws Exception {
     String program = funfloat;
-    program += "local float multiUnario <= 5.1 + 1.1|local float fl1|\n" +
-            "\tlocal float fl1<=56.6|";
-    program += "local float res <= 1.3|local bool mibool <= true|";
-    program += "res <= 2.2|arr[lit] <= 'c'|";
-    program += "local int in1<= --fl1 ~ -14/++in1+7- 15 **  arr[67] * func1 (hola, true, \"hola mundo\", 4.5, 'a') |";
-    program += "mibool <= (3.7 * fl1 + (-45.6~76)) < 56 ^ true # mibool # !mibool  | @semantico literal 76\n";
+    program += "local float res <= ++4.4|";
 
-    program += "miFunc(miFunc(a),'a')|";
-    program += "local bool bl0 <= 6.7 != 8.9|";
-    program += "local bool bl1 <= in1 => fl1 ^ false # !(func2(3,in1) > 56)|";
-
-    program += "local float res1 <= ++(1.34)|";
-    program += "local float res2 <= ++(((1.3)*(0.3)))|";
-    program += "local float res3 <= ++((1.1))|";
-    program += "local float res4 <= ++((1.1) - 23.1 + 1.0**2.0*5.0)|";
-    program += "local float res5 <= (1.3)|";
-    program += "local float res6 <= res1*1.0|";
-    program += "local float res7 <= (res1*1.0)|";
-    program += "local float res8 <= res1*1.0 + res2*1.0 + res3*1.0|";
-    program += "return (res*1.1 - ((23.1)) + ((res3 *1.2) ** res6 *1.3) * -5.4)|}";//res1+res2+res3
-    //program += "return 3.4|}";
-    program += "function float hola (int x, char y){" +
-            "\treturn b1|\n" +
-            "}";
+    program += "return (res*1.1 - ((23.1)) + ((res *1.2) ** res *1.3) * -5.4)|}";//res1+res2+res3
     program += funMain;
     
     Integer resultado = (Integer) testHelper(program).value;
@@ -255,7 +235,7 @@ public class ParserTest {
     String program = "";//funInt + "print(\"Soy la función hola\")|return 3|}";
     program += funMainAlt;
     //program += "local int temp1 <= 1 + 2 + 3 + -4 + 5 + 6|";//13✅
-    //program += "print(\"hola\")|";
+    program += "print(\"hola\")|";
     //program += "hola()|";
     //program += "print(temp1)|";
     //program += "local bool miBool <= 3333==3233|";
@@ -263,7 +243,7 @@ public class ParserTest {
 
     //program += "if if (4<6) {print(\"dentro del if\")|} else {print(\"Dentro del else\")|}";
     //program += "if (4<6) do {print(\"dentro del if\")|} elif(5==5) {print(\"Dentro del elif\")|}";
-    program += "if (4<6) {print(\"dentro del if\")|} elif(5==5) {print(\"Dentro del elif\")|} else {print(\"Dentro del else\")|}";
+    //program += "if (4<6) {print(\"dentro del if\")|} elif(5==5) {print(\"Dentro del elif\")|} else {print(\"Dentro del else\")|}";
     //program += "local bool miBool2 <= 3+1+3+4 == 5+53+1+3+4 =< 5+5 => 3|";
     //program += "local bool tuBool <= true|";
     //program += "print((1.1 + 3.3 + 5.5 + 7.7 + 9.9 - ( 1.1 + 3.3 + 5.5 + 7.7 + 9.9 + 1.0)))|";
@@ -271,11 +251,14 @@ public class ParserTest {
     //program += "print(5/5)|";
     //program += "print(\"Soy un string\")|";
 
-    program += "do {";
-    program += "print(\"Soy un do until\")|";
-    program += "}";
-    program += "until (9+1 == 9+1 == 9+1)|";
+    //program += "do {";
+    //program += "print(\"Soy un do until\")|";
+    //program += "}";
+    //program += "until (9+1 == 9+1 == 9+1)|";
 
+    program += "for (local int res <= 1| 2 < 5 | ++4) {";
+    program += "print(\"Soy un do print dentro de un for\")|break|}";
+    program += "print(\"adios!\")|";
     /*
     program += "local int temp2 <= 5 + 5 - 5 + 5 - 5 + 5|";//10✅
     program += "local int temp3 <= (5 + 5 + 5) + (5 + 5 + 5)|";//30✅
